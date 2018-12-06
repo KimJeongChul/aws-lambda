df9c214bec861705346d3f888e1e3b7da2d9adb6
KimJungChul <kjc5443@gmail.com>
2018-12-06 12:37:51 +0900

    Refactoring code

--- s3/lambda/video-processing/lambda_function.py
+++ s3/lambda/video-processing/lambda_function.py
@@ -1,12 +1,14 @@
 import boto3
 import os
 import sys
+import uuid
 import cv2
 
 s3_client = boto3.client('s3')
 
 TMP = "/tmp/"
 FILE_PATH_INDEX = 0
+FILE_NAME_INDEX = 2
 
 def video_processing(file_name, video_path):
     result_file_path = []
@@ -14,22 +16,29 @@ def video_processing(file_name, video_path):
     video = cv2.VideoCapture(video_path)
     idx = 0
 
+    print "Video Processing Start ..."
+
     while(video.isOpened()):
         ret, frame = video.read()
-        width = frame.get(3)
-        height = frame.get(4)
+        width = video.get(3)
+        height = video.get(4)
+
+        if idx == 100:
+            break
 
         if ret < 0:
             break
 
         frame = cv2.resize(frame, (720, 480))
-        result_path = video_path.split(".")[FILE_PATH_INDEX]+idx+".jpg"
+        result_path = video_path.split(".")[FILE_PATH_INDEX]+str(idx)+".jpg"
         result_file_path.append(result_path)
 
         cv2.imwrite(result_path, frame)
 
         idx += 1
 
+    vido.release()
+    print "Video Processing Done ..."
     return result_file_path
 
 def lambda_handler(event, context):
@@ -43,6 +52,7 @@ def lambda_handler(event, context):
 
         result_path = video_processing(file_name, download_path)
 
-        print result_path
         for upload_path in result_path:
-            s3_client.upload_file(upload_path, 'result-image-data-augmentation', upload_path.split("/")[FILE_NAME_INDEX])
+            s3_client.upload_file(upload_path, 'result-video-processing', upload_path.split("/")[FILE_NAME_INDEX])
+
+        print "S3 Upload Doen ..."
